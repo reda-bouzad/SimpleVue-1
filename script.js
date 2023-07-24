ed30e19a1fe17cd01307c9711f9856a941dd6d5f
reda-bouzad <redabouzadfst@gmail.com>
2023-07-24 13:04:44 +0100

    v4

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
-import data from "./data.js "
+import data from "./data.js ";
 const persons = [...data];
 
 const app = Vue.createApp({
-    // template: '<h1> Hello {{firstName}} </h1>',
-    // the data is a function that returns an object 
-    data() {
-        return {
-            firstName : persons[0].firstName,
-            lastName : persons[0].lastName,
-            email : persons[0].email,
-            gender : persons[0].gender,
-            picture : persons[0].picture,
-        }
-            
+  // template: '<h1> Hello {{firstName}} </h1>',
+  // the data is a function that returns an object
+  data() {
+    return {
+      firstName: persons[0].firstName,
+      lastName: persons[0].lastName,
+      email: persons[0].email,
+      gender: persons[0].gender,
+      picture: persons[0].picture,
+    };
+  },
+  methods: {
+    randomNumber() {
+      const randomNumber = Math.floor(Math.random() * persons.length);
+      return randomNumber;
     },
-    methods: {
 
-        randomNumber() {
-            const randomNumber = Math.floor(Math.random() * persons.length)  ;
-            const indexNumber = randomNumber;
-            console.log(indexNumber)
-            return randomNumber;
-        },
-
-        getUser() {
-            const indexNumber = this.randomNumber();
-            this.firstName = persons[indexNumber].firstName;
-            this.lastName = persons[indexNumber].lastName;
-            this.email = persons[indexNumber].email;
-            this.gender = persons[indexNumber].gender;
-            this.picture = persons[indexNumber].picture;
-        }
-        
+    /*
+    getUserFromFile() {
+      this.firstName = persons[this.randomNumber()].firstName;
+      this.lastName = persons[this.randomNumber()].lastName;
+      this.email = persons[this.randomNumber()].email;
+      this.gender = persons[this.randomNumber()].gender;
+      this.picture = persons[this.randomNumber()].picture;
     },
-})
+    */
 
-app.mount('#app')
+    getUsersFromApi() {
+      fetch("https://randomuser.me/api")
+        .then((res) => res.json())
+        .then((data) => {
+          this.firstName = data.results[0].name.first;
+          this.lastName = data.results[0].name.last;
+          this.email = data.results[0].email;
+          this.gender = data.results[0].gender;
+          this.picture = data.results[0].picture.large;
+        });
+    },
+  },
+});
+
+app.mount("#app");
